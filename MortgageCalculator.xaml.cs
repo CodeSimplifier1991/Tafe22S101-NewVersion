7899be483f682362081463c94eddff008eeb76f0
000951824 <behzad.pourdarab@student.tafesa.edu.au>
2022-04-01 11:45:43 +1030

    Developed the C# & XAML code of the Mortgage Calculator on the mortgageCalculatorFeature branch.

--- MortgageCalculator.xaml.cs
+++ MortgageCalculator.xaml.cs
@@ -26,5 +26,56 @@ namespace Calculator
 		{
 			this.InitializeComponent();
 		}
+
+		// START ---------- Loan Calculation Function -------------
+		private void calculateTheLoanRepayment() {
+
+
+			//Reading the data from the boxes
+			double principaleLoanAmount = double.Parse(principalLoanAmountBox.Text);
+			double durationOfLoanInYears = double.Parse(yearsOfTheLoanBox.Text);
+			double durationOfTheLoanInMonths = double.Parse(andMonthOfTheLoanBox.Text);
+			double yearlyInterestRate = double.Parse(yearlyInterestRateBox.Text) / 100;
+
+			//Calculating the monthly interest rate
+			double monthlyInterestRate = yearlyInterestRate / 12.0;
+
+			//Calculating the loan duration in Months
+			double loanRepayDurationInMonths = durationOfLoanInYears * 12 + durationOfTheLoanInMonths;
+
+			double monthlyRepayment = 0;
+
+
+			//Calculating the monthly repayment according to entered data
+			monthlyRepayment =
+				(principaleLoanAmount *
+				monthlyInterestRate *
+				(Math.Pow((1.0 + monthlyInterestRate), loanRepayDurationInMonths)) / ((Math.Pow((1.0 + monthlyInterestRate), loanRepayDurationInMonths)) - 1));
+
+			//Show the result in the boxes
+			monthlyInterestRateBox.Text = monthlyInterestRate.ToString("N4") + "%";
+			monthlyRepaymentBox.Text = monthlyRepayment.ToString("N");
+		}
+		// END ------------ Loan Calculation Function -------------
+
+
+
+		//START ---------- Calculate Button Click -----------
+		private void calculateBtn_Click(object sender, RoutedEventArgs e)
+		{
+			calculateTheLoanRepayment();
+		}
+		//END ------------ Calculate Button Click -----------
+
+
+
+		//START --------- Exit Button Click -----------
+		private void exitBtn_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+		//END ---------- Exit Button Click -----------
+
 	}
+	// MortgageCalculator
 }
